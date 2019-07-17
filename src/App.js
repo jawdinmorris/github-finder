f426ef34ffb2daec4f15439797e3b6c43a0fbc6b
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-17 14:00:07 +1000

    port10 complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar.js";
 
-class App extends React {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar />
+        <h1>Hello</h1>
       </div>
     );
   }
