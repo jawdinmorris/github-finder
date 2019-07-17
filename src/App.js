7ca61b6387c02f80d5fce182ef430eb302723d4b
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-17 15:33:15 +1000

    chap17

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar.js";
 import Users from "./components/users/Users.js";
+import Search from "./components/users/Search.js";
 import axios from "axios";
 
 class App extends Component {
@@ -25,6 +26,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
