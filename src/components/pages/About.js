0c9c0fbfeea25198106eb3ff8e023209d51f9888
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-17 16:17:55 +1000

    chap21

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this Project</h1>
+      <p>App to search Github users.</p>
+      <p>V0.0.9</p>
+    </Fragment>
+  );
+};
+
+export default About;
