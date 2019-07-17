86d583fead48b076a2b043c8c18695f59a17435f
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-17 15:51:37 +1000

    chap19

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ export class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -20,6 +22,7 @@ export class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
@@ -36,6 +39,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-white btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
