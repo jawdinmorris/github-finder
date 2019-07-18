f3eaa3204f30aa0391c004cde66f53f2a68e91ba
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-19 08:44:40 +1000

    act chap30

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -21,7 +21,19 @@ const GithubState = props => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  //Search Users
+  //Search Github Users
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data
+    });
+  };
 
   //Get User
 
@@ -30,6 +42,7 @@ const GithubState = props => {
   //Clear Users
 
   //Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -37,7 +50,8 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
